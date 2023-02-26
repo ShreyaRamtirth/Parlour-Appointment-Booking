1cc34eee6387ae15334cf8269fd1030bf515b73a
Shreya Ramtirth <shreyaramtirth4@gmail.com>
2023-02-26 21:49:43 +0530

    frontend commit package added

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,137 +1,17 @@
-import React from 'react';
+import React ,{useRef, useState, useEffect} from 'react';
 import Link from 'next/link';
 
 const index = () => {
     return (
         <div className='bg-success p-2 text-dark bg-opacity-10' >
-            
 		<section className="small-section bg-section-color3 opacity-20" data-background="/images/section-bg-3.jpg">
-			<div className="container">
-
-		
+			<div className="container-fluid mt-20">	
             <h3 className="blog-title font-alt align-center mb-5">Admin Menus Click To Explore More</h3>
-				{/* <div className="row">
-					<div className="col-lg-3 col-md-6 mb-lg-30">
-						<div className="gift-item alt-1 font-alt">
-							<div className="gift-wrapper">
-								<div className="gift-media">
-									<a href="#">
-										<img src="/images/img4.jpg" data-at2x="images/gifts/img-1@2x.jpg" />
-									</a>
-									<div className="gift-inside">
-										<div className="gift-title">
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-                                            <Link href='admin/appointment_report'><h3>Reports</h3></Link>
-										</div>
-										
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 col-md-6 mb-lg-30">
-						<div className="gift-item alt-1 font-alt">
-							<div className="gift-wrapper">
-								<div className="gift-media">
-									<a href="#">
-										<img src="/images/img-2.jpg" data-at2x="images/gifts/img-2@2x.jpg"  />
-									</a>
-									<div className="gift-inside">
-										<div className="gift-title">
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-											<Link href='admin/add_services'><h3>Services</h3></Link>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 col-md-6 mb-lg-30">
-						<div className="gift-item alt-1 font-alt">
-							<div className="gift-wrapper">
-								<div className="gift-media">
-									<a href="#">
-										<img src="/images/img3.jpg" data-at2x="images/gifts/img-3@2x.jpg"  />
-									</a>
-									<div className="gift-inside">
-										<div className="gift-title">
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-											
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-											<Link href='admin/acceptAppointment'><h3>Appointment</h3></Link>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 col-md-6">
-						<div className="gift-item alt-1 font-alt">
-							<div className="gift-wrapper">
-								<div className="gift-media">
-									<a href="#">
-										<img src="/images/img-4.jpg" data-at2x="images/gifts/img-4@2x.jpg"  />
-									</a>
-									<div className="gift-inside">
-										<div className="gift-title">
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-											
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-                                            <Link href='admin/add_beautition'><h3>Beautician</h3></Link>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 col-md-6">
-						<div className="gift-item alt-1 font-alt">
-							<div className="gift-wrapper">
-								<div className="gift-media">
-									<a href="#">
-										<img src="/images/img-15.jpg" data-at2x="images/gifts/img-4@2x.jpg"  />
-									</a>
-									<div className="gift-inside">
-										<div className="gift-title">
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-											
-											<div className="img-line">
-												<img src="/images/img-line-26.png" data-at2x="images/lines/img-line-26@2x.png"  />
-											</div>
-                                            <Link href='admin/add_offers'><h3>Offers</h3></Link>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div> */}
-
-		
-<iframe styles={{background: "#F1F5F4",border: "none",borderRadius: "2px",boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)", width: "100vw" ,height: "100vh" }}
-src="https://charts.mongodb.com/charts-stock-market-bbrtd/embed/dashboards?id=63e603e3-91d7-4683-81ff-466215bec614&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=false&scalingWidth=scale&scalingHeight=fixed"></iframe>
-
-
+			<iframe
+			width={1400}
+			height={1000}
+			src="https://charts.mongodb.com/charts-project-0-nlfkj/embed/dashboards?id=63e609c8-6c3f-4966-8b03-bc22405376db&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed">
+			</iframe>
 			</div>
 		</section>
         </div>
